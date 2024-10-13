8bf5cc6e149367e890ce95f0090f1b59fff240ac
almog <almogluzon@gmail.com>
2024-10-13 12:37:08 +0300

    Added a variable to hold all the checkpoints

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const express = require('express');
-const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Main Page');
-});
+const express = require('express');
+const app = express();
+app.use(express.json());
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+let checkPoints = [];
+
